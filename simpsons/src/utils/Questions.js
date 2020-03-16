a31f7273b37399e5ed66b7491496d4e22e30bbd6
skuttenkuler <sam.kuttenk@gmail.com>
2020-03-15 22:12:56 -0700

    intial commit, folder set up

---
+++ simpsons/src/utils/Questions.js
@@ -0,0 +1,56 @@
+let Questions = [
+    {
+      title: "Which town does the Simpsons take place? ",
+      choices: ["Nashville", "Springfield", "Springton", "Portland"],
+      answer: "Springfield"
+    },
+    {
+      title: "Which is NOT one of Bart's catchphrases?",
+      choices: ["Eat my shorts!", "Don't have a cow man", "Aye carumba", "Woohoo!"],
+      answer: "Woohoo!"
+    },
+    {
+        title: "Whoe runs the Kwik-E-Mart?",
+        choices: ["Apu", "Mr. Burns", "Barney", "Homer"],
+        answer: "Apu"
+    },
+    {
+        title: "What's the name of Mr. Burns bear",
+        choices: ["Coco", "Popo", "Bobo", "Slomo"],
+        answer: "Bobo"
+    },
+    {
+        title: "What did the Simpsons get for their first Christmas?",
+        choices: ["A car", "A grill", "A cat", "A dog"],
+        answer: "A dog"
+    },
+
+    {
+      title: "What's the name of the school's bus driver",
+      choices: ["Cletus", "Troy McClure", "Homer", "Otto"],
+      answer: "Otto"
+    },
+    {
+      title: "What is Marge's maiden name?",
+      choices: ["Hoover", "Bouvier", "Smith", "Croupier"],
+      answer: "Bouvier"
+    },
+    {
+      title: "Who practices medicine without a license?",
+      choices: ["Dr. Hoover ", "Dr. Hibbert", "Dr. Wiggum", "Dr. Nick"],
+      answer: "Dr. Nick"
+    },
+    {
+      title: "Who stands in for Kent Brockman when he is ill?",
+      choices: ["Scott Christian", "Arnie Pie", "Rainier Wolfcastle", "Slomo"],
+      answer: "Scott Christian"
+    },
+    {
+      title: "Who invented the Flaming Moe?",
+      choices: ["Homer Simpson", "Moe Szyslak", "Moe St. Cool", "Selma Bouvier"],
+      answer: "Homer Simpson"
+    },
+
+  ];
+
+  export default Questions
